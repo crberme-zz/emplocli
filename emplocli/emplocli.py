0f3b8895626e274528f7299a68dd4dc96fdcbc2e
Cristian Bermejo <22524108+crberme@users.noreply.github.com>
2020-07-02 17:22:59 +0200

    Code cleanup

--- emplocli/emplocli.py
+++ emplocli/emplocli.py
@@ -1,89 +1,117 @@
 from argparse import ArgumentParser
 from data import url, db, username, password
 from xmlrpc import client
+from xmlrpc.client import ProtocolError
 import logging
 
-# Initialize the argument parser
-parser = ArgumentParser(description="Register attendances on an Odoo instance.")
-args_group = parser.add_mutually_exclusive_group()
-args_group.add_argument("--check-in", "-i", dest="check", action="store_const", const=1, help="Register a check in.")
-args_group.add_argument("--check-out", "-o", dest="check", action="store_const", const=2, help="Register a check out.")
-parser.add_argument("--reason", "-r", help="Specify the attendance reason ID for check out.")
-parser.add_argument("--list-reasons", "-R", action="store_true", help="List the available reason ID with a brief description.")
-args = parser.parse_args()
+def read_arguments():
+    parser = ArgumentParser(description="Register attendances on an Odoo instance.")
+    args_group = parser.add_mutually_exclusive_group()
+    args_group.add_argument("--check-in", "-i", dest="check", action="store_const", const=1, help="Register a check in.")
+    args_group.add_argument("--check-out", "-o", dest="check", action="store_const", const=2, help="Register a check out.")
+    parser.add_argument("--reason", "-r", help="Specify the attendance reason ID for check out.")
+    parser.add_argument("--list-reasons", "-R", action="store_true", help="List the available reason ID with a brief description.")
+    return parser.parse_args()
 
-# Initialize the logger
-logging.basicConfig(filename=__file__[:__file__.rfind(".")] + ".log", level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+class EmployeeClient:
+    def __init__(self):
+        self.common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        self.models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        self.id = None
+        self.attendance_state = None
 
-# Log in
-common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, password, {})
+    def login(self):
+        self.uid = self.common.authenticate(db, username, password, {})
+        if not self.uid:
+            return False
+        else:
+            return True
 
-# Perform the requested action
-if uid:
-    models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    def get_reasons(self):
+        return self.models.execute_kw(db, self.uid, password, 'hr.attendance.reason', 'search_read', [[]], {"fields": ["name"]})
 
-    if args.list_reasons:
-        response = models.execute_kw(db, uid, password, 'hr.attendance.reason', 'search_read', [[]], {"fields": ["name"]})
-        print("Available reason IDs:")
-        print("ID\tName")
-        print(("=" * max(len(str(response_item["id"])) for response_item in response)) + "\t" + ("=" * max(len(response_item["name"]) for response_item in response)))
+    def register_attendance(self):
+        return self.models.execute_kw(db, self.uid, password, 'hr.employee', 'attendance_manual', [self.id, False])
+
+    def register_attendance_reason(self, attendance_id, reason):
+        # [6, False, [IDs]] deletes the current IDs and inserts the provided one(s) in a single operation
+        return self.models.execute_kw(db, self.uid, password, 'hr.attendance', 'write', [[attendance_id], {'attendance_reason_ids': [[6, False, [reason]]]}])
+
+    def check_in(self):
+        attendance_state = self.get_attendance_state()
+        if attendance_state == "checked_in":
+            logging.info("User already checked in.")
+        else:
+            try:
+                action = self.register_attendance()
+                logging.info("Checked in.")
+            except ProtocolError as err:
+                logging.error(err.errmsg)
+
+    def check_out(self, reason=None):
+        attendance_state = self.get_attendance_state()
+        if attendance_state == "checked_out":
+            logging.info("User not checked in.")
+        else:
+            try:
+                response = self.register_attendance()
+                logging.info("Checked out.")
+            except ProtocolError as err:
+                logging.error(err.errmsg)
+                return
+
+            if reason:
+                if any(reasons.get("id") == reason for reasons in self.get_reasons()):
+                    try:
+                        self.register_attendance_reason(response.get("action").get("attendance").get("id"), reason)
+                        logging.info("Attendance reason set.")
+                    except ProtocolError as err:
+                        logging.error(err.errmsg)
+                        return
+                else:
+                    logging.error("Unknown reason ID \"%s\", please use the --list-reasons or -R flags to get a list of available reason IDs.", args.reason)
+
+
+    def get_attendance_state(self):
+        response = self.models.execute_kw(db, self.uid, password, 'hr.employee', 'search_read', [[['user_id', '=', self.uid]]], {'fields': ['attendance_state']})
         for response_item in response:
-            print(str(response_item.get("id")) + "\t" + response_item.get("name"))
-    else:
-        # Get the current attendance_state
-        response = models.execute_kw(db, uid, password, 'hr.employee', 'search_read', [[['user_id', '=', uid]]], {'fields': ['attendance_state']})
-        employee_id = None
-        for response_item in response:
-            # Failsafe in case we get more than one result
-            if employee_id is not None:
-                logging.error("Unexpected result when quering for current attendance state.")
-                employee_id = None
-                break
+            if self.id:
+                logging.warn("Overriding ID with value \"%i\", original value was \"%i\"", response_item.get("id"), self.id)
 
-            employee_id = response_item.get("id")
+            self.id = response_item.get("id")
+            
+            if self.attendance_state:
+                logging.warn("Overriding attendance state with value \"%s\", original value was \"%s\"", response_item.get("attendance_state"), self.attendance_state)
+
             attendance_state = response_item.get("attendance_state")
 
-        if employee_id:
-            if args.check == 1:
-                # Check in
-                if attendance_state == "checked_in":
-                    logging.info("User already checked in.")
-                elif attendance_state == "checked_out":
-                    # Register the attendance
-                    response = models.execute_kw(db, uid, password, 'hr.employee', 'attendance_manual', [employee_id, False])
-                    action = response.get("action")
-                    attendance = action.get("attendance")
-                    logging.info("Checked in.")
-                else:
-                    logging.error("Unexpected attendance state: \"%s\".", attendance_state)
-            elif args.check == 2:
-                # Check out
-                if attendance_state == "checked_out":
-                    logging.info("User already checked out.")
-                elif attendance_state == "checked_in":
-                    # Register the attendance
-                    response = models.execute_kw(db, uid, password, 'hr.employee', 'attendance_manual', [employee_id, False])
-                    action = response.get("action")
-                    attendance = action.get("attendance")
-                    logging.info("Checked out.")
+        return attendance_state
 
-                    if args.reason:
-                        # Set a reason for the registered attendance
-                        reason = int(args.reason)
-                        response = models.execute_kw(db, uid, password, 'hr.attendance.reason', 'search_read', [[]], {"fields": ["id"]})
-                        if not any(reasons.get("id") == reason for reasons in response):
-                            logging.error("Unknown reason ID \"%s\", please use the --list-reasons or -R flags to get a list of available reason IDs.", args.reason)
-                        else:
-                            # [6, False, [IDs]] deletes the current IDs and inserts the provided one(s) in a single operation
-                            response = models.execute_kw(db, uid, password, 'hr.attendance', 'write', [[attendance.get("id")], {'attendance_reason_ids': [[6, False, [reason]]]}])
-                            if response:
-                                logging.info("Attendance reason set to %s.", args.reason)
-                            else:
-                                logging.error("Couldn't set attendance reason due to an unknown error.")
-                else:
-                    logging.error("Unexpected attendance state: \"%s\".", attendance_state)
+
+if __name__ == "__main__":
+    logging.basicConfig(filename=__file__[:__file__.rfind(".")] + ".log", level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+
+    # Initialize variables
+    args = read_arguments()
+    client = EmployeeClient()
+    
+    # Perform action
+    if client.login():
+        if args.list_reasons:
+            reasons = client.get_reasons()
+
+            print("Available reason IDs:")
+            print("ID\tName")
+            print(("=" * max(len(str(reason["id"])) for reason in reasons)) + "\t" + ("=" * max(len(reason["name"]) for reason in reasons)))
+            for reason in reasons:
+                print(str(reason.get("id")) + "\t" + reason.get("name"))
         else:
-            logging.error("None or multiple employee ID(s) found for user \"%s\".", username)
-else:
-    logging.error("Login failed.")
+            if args.check == 1:
+                client.check_in()
+            elif args.check == 2:
+                if args.reason:
+                    client.check_out(int(args.reason))
+                else:
+                    client.check_out()
+    else:
+        logging.error("Login failed.")
